67465d15e1b194e1fb74ef9dc060ceada84cf8ae
yangnianbing <137602812@qq.com>
2017-12-10 22:02:28 +0800

    :coffee:

--- src/indexdb/lib.js
+++ src/indexdb/lib.js
@@ -1,5 +1,35 @@
+var indexedDB = window.indexedDB
+
 var wrap = {
-    request (req) {
-        
+    request (dbRequest) {
+        return new Promise((resolve, reject) => {
+            dbRequest.onsuccess = (e) => {
+                resolve(e.target.result)
+            }
+
+            dbRequest.onerror = (e) => {
+                reject(e)
+            }
+        })        
+    },
+    transaction (dbTransaction) {
+        return {
+            objectStore (name) {
+                return wrap.objectStore(dbTransaction.objectStore(name));
+            }
+        }
+    },
+
+    objectStore (dbObjectStore) {
+        var store = {};
+        var operas = ['add', 'clear', 'count', 'createIndex', 'delete', 'deleteIndex', 'get', 'getKey', 'getAll', 'getAllKeys', 'index', 'openCursor', 'openKeyCursor', 'put']
+
+        operas.forEach( (opera) => {
+            store[opera] = (opera => {
+                return (...args) => wrap.request((args) => dbObjectStore[opera].apply(dbObjectStore, args));
+            })(opera)
+        });
+        return store;
     }
+    
 }
