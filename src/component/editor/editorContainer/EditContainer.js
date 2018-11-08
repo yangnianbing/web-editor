38ce9ccbdbbfaf48776bfd4e3e6d4bb274513cbf
yangnb <yangnb@xiaoshouyi.com>
2018-11-08 14:42:40 +0800

    css

--- src/component/editor/editorContainer/EditContainer.js
+++ src/component/editor/editorContainer/EditContainer.js
@@ -6,28 +6,40 @@ export default class EditContainer extends Component{
     state={
         file:{},
         _openViews: {},
-        container: <div class="editor-container"></div>
+        editorInstance:null,
+        editorInstanceView:null,
     }
     componentDidMount(){
         var $component = this;
+        var $editContainer = document.querySelector('.editor-container');
         eventBus.on('open.file', ({file}) => {
             if(file.path === $component.state.file.path){
                 return;
             }
             $component.setState({file: file})
             if($component.state._openViews[$component.state.file.path]){
-                $component.state.container.appendChild($component.state._openViews[$component.state.file.path]);
+                $editContainer.appendChild($component.state._openViews[$component.state.file.path].editorInstanceView);
             }else{
-                // var $editorInstance =  document.querySelector('.editor-container .editor-instance');
-                // $component.state._openViews[$component.state.file.path] = ;
-                // var editor = monaco.editor.create(document.querySelector('.editor-container'), {
-                //     value: file.content,
-                //     language: 'javascript'
-                // });
+                if($component.state.editorInstanceView){
+                    $editContainer.removeChild($component.state.editorInstanceView);
+                }
+                var newEditInstanceView = document.createElement('div');
+                newEditInstanceView.className = 'edit-instance';
+                $editContainer.appendChild(newEditInstanceView);
+                var newEditInstance = monaco.editor.create(newEditInstanceView, {
+                    value: file.content,
+                    language: 'javascript'
+                });
+                $component.setState({editorInstance: newEditInstance, editorInstanceView: newEditInstanceView});
+                $component.state._openViews[$component.state.file.path] = {
+                    editorInstance: newEditInstance,
+                    editorInstanceView: newEditInstanceView
+                }
+
             }
         })
     }
     render(){
-        return this.state.container;
+        return <div class="editor-container"></div>;
     }
 }
